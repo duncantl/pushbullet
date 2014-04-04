a6bde9a0a1c1d236932256fb5d75b5d96475fabd
Duncan Temple Lang <duncan@r-project.org>
2014-04-03 18:00:07 -0700

    upload files

--- pushbullet.R
+++ pushbullet.R
@@ -1,3 +1,10 @@
+# Copyright Duncan Temple Lang 2014
+# License: BSD_3_Clause license.
+#
+# Based on a query from Dirk Eddelbuettel
+# and taken from the pushbullet.com API examples
+# https://www.pushbullet.com/api
+
 library(RCurl) 
 library(RJSONIO) # or json or JSONlite
 
@@ -33,10 +40,18 @@ function(device, body, title, type = "note",
          u = "https://api.pushbullet.com/api/pushes",
          curl = getPushBulletHandle(key))
 {
-  params = list(device_iden = device, type = type, title = title, body = body)
-  ans = postForm(u, .params = params, style = "POST", 
-                  .opts = list(...), curl = curl)
+  params = list(device_iden = device, type = type, title = title)
+  if(type == "file") {
+    params$file = fileUpload(body) 
+    .opts = list(...)
+    ans = postForm(u, .params = params, style = "HTTPPOST", .opts = .opts, curl = curl)
+  } else {
+    params$body = body
+    ans = postForm(u, .params = params, style = "POST", .opts = list(...), curl = curl)
+  }
   fromJSON(ans)
 }
 
 #curl https://api.pushbullet.com/api/pushes -v -u key:  -d device_iden=u1qSJddxeKwOGuGW  -d type=note  -d title=Title  -d body=Body  -X POST
+
+# curl https://api.pushbullet.com/api/pushes -u API_KEY:  -F device_iden=u1qSJddxeKwOGuGW -F type=file -F file=@pushbullet.R
